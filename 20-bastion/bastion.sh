7682d6ca5b60903b21a0d3d3b703f46b776a3ee4
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-14 16:52:43 +0530

    added files

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -30,6 +30,11 @@ else
     echo "You are super user."
 fi
 
+# mysql
+
+dnf install mysql -y
+VALIDATE $? "MySql installation"
+
 # docker
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
